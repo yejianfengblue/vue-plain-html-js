5f3c3b42346b6d8b88a1fcd57514b0d021e6aa80
Jian Feng Ye <yejianfengblue@163.com>
2020-12-09 16:01:39 +0800

    Event handling

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@ const Counter = {
     data() {
         return {
             counter: 0,
-            counterMessage: 'The counter increments every 1 second. You loaded this page on '
+            message: 'The counter increments every 1 second. You loaded this page on '
                 + new Date().toLocaleString()
         }
     },
@@ -10,6 +10,11 @@ const Counter = {
         setInterval(() => {
             this.counter++
         }, 1000)
+    },
+    methods: {
+        resetCounter() {
+            this.counter = 0
+        }
     }
 }
 
